32e71cf30ed4e35b23da1497cbccf3d989290f94
edlea <ed@edlea.net>
2020-07-01 17:06:09 +0100

    Correct target and current temperatures
    They were the wrong way round before

--- index.js
+++ index.js
@@ -4,131 +4,162 @@ const md5 = require("md5");
 const xml = require("fast-xml-parser");
 const bent = require("bent");
 const get = bent(
-  "https://emea-salprod02-api.arrayent.com:8081/zdk/services/zamapi/",
-  "string"
+    "https://emea-salprod02-api.arrayent.com:8081/zdk/services/zamapi/",
+    "string"
 );
 
 const APPID = "1097";
-const ATTRIBUTES = { NAME: 2287, SUMMARY: 2257 };
+const ATTRIBUTES = {
+    NAME: 2287,
+    SUMMARY: 2257
+};
 const MODES = [
-  "OFFLINE",
-  "AUTO_HIGH",
-  "AUTO_MEDIUM",
-  "AUTO_LOW",
-  "HIGH",
-  "MEDIUM",
-  "LOW",
-  "PARTY",
-  "AWAY",
-  "FROST",
-  "ON",
+    "OFFLINE",
+    "AUTO_HIGH",
+    "AUTO_MEDIUM",
+    "AUTO_LOW",
+    "HIGH",
+    "MEDIUM",
+    "LOW",
+    "PARTY",
+    "AWAY",
+    "FROST",
+    "ON",
 ];
 
 const summaryToValues = (summary) => ({
-  target: (summary.charCodeAt(2) - 32) * 0.5,
-  current: (summary.charCodeAt(3) - 32) * 0.5,
-  mode: MODES[summary.charCodeAt(1) - 32],
+    current: (summary.charCodeAt(2) - 32) * 0.5,
+    target: (summary.charCodeAt(3) - 32) * 0.5,
+    mode: MODES[summary.charCodeAt(1) - 32],
 });
 
 class Salus {
-  constructor({ username, password }) {
-    this.username = username;
-    this.password = password;
-  }
+    constructor({
+        username,
+        password
+    }) {
+        this.username = username;
+        this.password = password;
+    }
 
-  async login() {
-    const loginResponse = await get(
-      `userLogin?appId=${APPID}&name=${this.username}&password=${md5(
+    async login() {
+        const loginResponse = await get(
+            `userLogin?appId=${APPID}&name=${this.username}&password=${md5(
         this.password
       )}&timestamp=${new Date().getTime()}`
-    );
-    const userLoginResponse = xml.parse(loginResponse, {
-      ignoreNameSpace: true,
-    });
-    this.session = userLoginResponse.userLoginResponse;
+        );
+        const userLoginResponse = xml.parse(loginResponse, {
+            ignoreNameSpace: true,
+        });
+        this.session = userLoginResponse.userLoginResponse;
 
-    const devicesResponse = await get(
-      `getDeviceList?secToken=${this.session.securityToken}&userId=${
+        const devicesResponse = await get(
+            `getDeviceList?secToken=${this.session.securityToken}&userId=${
         this.session.userId
       }&timestamp=${new Date().getTime()}`
-    );
-    this.device = xml.parse(devicesResponse, {
-      ignoreNameSpace: true,
-    }).getDeviceListResponse.devList;
-  }
+        );
+        this.device = xml.parse(devicesResponse, {
+            ignoreNameSpace: true,
+        }).getDeviceListResponse.devList;
+    }
 
-  baseParameters() {
-    return `secToken=${this.session.securityToken}&userId=${
+    _baseParameters() {
+        return `secToken=${this.session.securityToken}&userId=${
       this.session.userId
-    }&timestamp=${new Date().getTime()}`;
-  }
+    }&devId=${this.device.devId}&timestamp=${new Date().getTime()}`;
+    }
 
-  async devices() {
-    if (!this.session) await this.login();
-    const response = await get(
-      `getDeviceAttributesWithValues?${this.baseParameters()}&deviceTypeId=1&devId=${
-        this.device.devId
-      }`
-    );
-    const deviceAttributes = xml.parse(response, { ignoreNameSpace: true })
-      .getDeviceAttributesWithValuesResponse.attrList;
+    async _request({
+        method,
+        parameters
+    }) {
+        if (!this.session) await this.login();
+        const request = `${method}?${this._baseParameters()}&${parameters}`;
+        try {
+            const response = await get(request);
+            return xml.parse(response, {
+                ignoreNameSpace: true
+            });
+        } catch (e) {
+            // Do it again, once, to avoid an infinite loop
+            if (!this.session) await this.login();
+            const response = await get(request);
+            return xml.parse(response, {
+                ignoreNameSpace: true
+            });
+        }
+    }
 
-    const namesAttribute = deviceAttributes.find(
-      (attribute) => attribute.id == ATTRIBUTES["NAME"]
-    );
-    const summary = deviceAttributes
-      .find((attribute) => attribute.id == ATTRIBUTES["SUMMARY"])
-      .value.match(/.{1,8}/g)
-      .map((a) => ({ id: a.substring(0, 4), value: a.substring(4) }));
+    async devices() {
+        const response = await this._request({
+            method: "getDeviceAttributesWithValues",
+            parameters: `deviceTypeId=1`,
+        });
+        const deviceAttributes =
+            response.getDeviceAttributesWithValuesResponse.attrList;
 
-    const devices = namesAttribute.value
-      .replace(/,$/, "")
-      .split(",")
-      .map((n) => ({
-        id: n.substring(0, 4),
-        name: n.substring(4),
-        ...summaryToValues(
-          summary.find((s) => s.id == n.substring(0, 4)).value
-        ),
-      }));
-    return devices;
-  }
+        const namesAttribute = deviceAttributes.find(
+            (attribute) => attribute.id == ATTRIBUTES["NAME"]
+        );
+        const summary = deviceAttributes
+            .find((attribute) => attribute.id == ATTRIBUTES["SUMMARY"])
+            .value.match(/.{1,8}/g)
+            .map((a) => ({
+                id: a.substring(0, 4),
+                value: a.substring(4)
+            }));
 
-  async setTarget({ id, temperature }) {
-    if (!this.session) await this.login();
-    const value = `!${id}${String.fromCharCode(temperature * 2 + 32)}`;
-    console.log(`value: ${value} encoded: ${encodeURIComponent(value)}`);
-    const result = await get(
-      `setMultiDeviceAttributes2?${this.baseParameters()}&devId=${
-        this.device.devId
-      }&name1=B06&value1=${encodeURIComponent(value)}`
-    );
-    return result;
-  }
+        const devices = namesAttribute.value
+            .replace(/,$/, "")
+            .split(",")
+            .map((n) => ({
+                id: n.substring(0, 4),
+                name: n.substring(4),
+                ...summaryToValues(
+                    summary.find((s) => s.id == n.substring(0, 4)).value
+                ),
+            }));
+        return devices;
+    }
 
-  async setMode({ id, mode, duration }) {
-    if (!this.session) await this.login();
-    /*
-        35 - # - AUTO
-        36 - $ - HIGH
-        37 - % - MEDIUM
-        38 - & - LOW
-        39 - ' - PARTY (followed by zeropadded number of hours)
-        40 - ( - AWAY (followed by zeropadded number of days)
-        41 - ) - FROST
-      */
-    const MODES = ["AUTO", "HIGH", "MEDIUM", "LOW", "PARTY", "AWAY", "FROST"];
-    const value = `!${id}${String.fromCharCode(MODES.indexOf(mode) + 35)}${
+    async setTarget({
+        id,
+        temperature
+    }) {
+        const value = `!${id}${String.fromCharCode(temperature * 2 + 32)}`;
+        console.log(value);
+        const result = await this._request({
+            method: "setMultiDeviceAttributes2",
+            parameters: `name1=B06&value1=${encodeURIComponent(value)}`,
+        });
+        return result;
+    }
+
+    async setMode({
+        id,
+        mode,
+        duration
+    }) {
+        /*
+            35 - # - AUTO
+            36 - $ - HIGH
+            37 - % - MEDIUM
+            38 - & - LOW
+            39 - ' - PARTY (followed by zeropadded number of hours)
+            40 - ( - AWAY (followed by zeropadded number of days)
+            41 - ) - FROST
+          */
+        const MODES = ["AUTO", "HIGH", "MEDIUM", "LOW", "PARTY", "AWAY", "FROST"];
+        const value = `!${id}${String.fromCharCode(MODES.indexOf(mode) + 35)}${
       duration ? duration.toString().padStart(2, "0") : ""
     }`;
-    console.log(`value: ${value} encoded: ${encodeURIComponent(value)}`);
-    const result = await get(
-      `setMultiDeviceAttributes2?${this.baseParameters()}&devId=${
-        this.device.devId
-      }&name1=B05&value1=${encodeURIComponent(value)}`
-    );
-    return result;
-  }
+        console.log(value);
+        const result = await this._request({
+            method: "setMultiDeviceAttributes2",
+            parameters: `name1=B05&value1=${encodeURIComponent(value)}`,
+        });
+        return result;
+    }
 }
 
 module.exports = Salus;
